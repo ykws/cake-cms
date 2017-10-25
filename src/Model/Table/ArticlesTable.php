586243ead63633cfc160126d112742f6c1f21bff
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-10-25 10:54:50 +0900

    Update Validation Rules for Articles

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -4,6 +4,7 @@ namespace App\Model\Table;
 
 use Cake\ORM\Table;
 use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table
 {
@@ -19,5 +20,18 @@ class ArticlesTable extends Table
             $sluggedTitle = Text::slug($entity->title);
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+    }
+    
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->notEmpty('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+            
+            ->notEmpty('body')
+            ->minLength('body', 10);
+        
+        return $validator;
     }
 }
