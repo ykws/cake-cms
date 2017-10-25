55428fae60c300573f7b35cab9e4ab74fb65a223
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-10-25 10:31:00 +0900

    Adding Articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,9 +4,16 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+        
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+    
     public function index()
     {
-        $this->loadComponent('Paginator');
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
@@ -15,5 +22,22 @@ class ArticlesController extends AppController
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
+    }
+    
+    public function add()
+    {
+        $article = $this->Articles->newEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            
+            $article->user_id = 1;
+            
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
     }
 }
